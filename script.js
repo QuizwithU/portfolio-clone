36eb7b5ee7bc25542af4b1787e5804fd71a9f936
QuizwithU <88932522+QuizwithU@users.noreply.github.com>
2025-07-11 21:26:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Blogger", "Web_Designer"],
+        strings: ["Web_developer", "SQA_developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Blogger", "Web_Designer"],
+        strings: [ "Web_developer", "SQA_developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
